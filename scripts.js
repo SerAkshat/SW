f5ad34b28f64e647f6b52b85862925c438bd1724
SerAkshat <ak.gupta.main@gmail.com>
2024-10-25 21:54:13 +0530

    2nd

---
+++ scripts.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+    const section = document.querySelector('.section');
+    const war = document.querySelector('.war');
+    const collide = document.querySelector('.collide');
+    const thunderbolt = document.querySelector('.thunderbolt');
+    const buttonContainer = document.querySelector('.button-container');
+
+    section.style.display = 'none';
+    war.style.display = 'none';
+    collide.style.display = 'none';
+    thunderbolt.style.display = 'none';
+    buttonContainer.style.display = 'none';
+
+    setTimeout(() => {
+        section.style.display = '';
+        war.style.display = '';
+        collide.style.display = '';
+        thunderbolt.style.display = '';
+    }, 50);
+
+
+    setTimeout(() => {
+        buttonContainer.style.display = 'flex';
+    }, 3000);
+};
+
+setTimeout(() => {
+    document.querySelector('.section').style.display = 'none';
+    document.querySelector('.war').style.display = 'none';
+}, 2500);
